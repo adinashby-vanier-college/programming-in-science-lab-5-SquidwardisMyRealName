8b3526d77b100e36bb0e63f52aba76014563ab1c
SquidwardisMyRealName <coddywang@gmail.com>
2026-01-07 12:01:49 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,43 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = "*" * n + ("\n*" + " " * (n-2) + "*") * (n-2)
+    if n > 1:
+        result += "\n" + "*" * n
+
+    return result  
+
+# natural_number = int(input("Enter a natural number: "))
+# hollow_square(natural_number)
+# print(hollow_square(natural_number))
+
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    return "" 
+
+
+
+
+# natural_number = int(input("Type a natural number: "))
+# number_pattern(natural_number)
+# print(number_pattern(natural_number))
+
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    result = 0
+    for natural_numbers in range(0, n + 1): 
+        result += natural_numbers 
+    return result    
+
+# natural_number = int(input("Enter a natural number: "))
+# sum_of_natural_numbers(natural_number)
+# print(sum_of_natural_numbers(natural_number)) 
+
 
 # Example for n = 4:
 #    *
@@ -24,4 +49,9 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ""
+
+
+# natural_number = int(input("Enter a natural number: "))
+# centered_star_pyramid(natural_number)
+# print(centered_star_pyramid(natural_number))
